b7da45bdf1d928d7468b40a27ff019ca07dae0bc
Walid Berrouk <jw_berrouk@esi.dz>
2022-01-03 17:55:05 +0100

    upgrading gallery, fixing section-header size, changing style for footer links

--- js/main.js
+++ js/main.js
@@ -155,22 +155,14 @@ jQuery(document).ready(function( $ ) {
 
   // Gallery carousel (uses the Owl Carousel library)
   $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
+    margin: 15,
     loop: true,
-    center:true,
     responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
     }
   });
 
-  // Buy tickets select the ticket type on click
-  $('#buy-ticket-modal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var ticketType = button.data('ticket-type');
-    var modal = $(this);
-    modal.find('#ticket-type').val(ticketType);
-  })
-
-// custom code
+  var element = document.getElementsByClassName("owl-item");
+  element.classList.remove("cloned");
+  element.classList.remove("active");
 
 });
